c597c7fec65992a117929ad3e1ff174db7c68b55
Neil Fitzgerald <neilfitz@Neils-MacBook-Pro.local>
2014-04-16 16:20:53 -0400

    Modified marker icon, added popups

--- wanderment/static/js/maps.js
+++ wanderment/static/js/maps.js
@@ -1,16 +1,30 @@
 function initialize() {
 	var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
 	var mapOptions = {
-	  zoom: 4,
-	  center: myLatlng
+	        zoom: 4,
+		center: myLatlng
 	}
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	
 	// To add the marker to the map, use the 'map' property
 	var marker = new google.maps.Marker({
 		position: myLatlng,
+		icon: {
+		        path: google.maps.SymbolPath.CIRCLE,
+			scale: 5,
+			fillColor: "#3D0099",
+			strokeColor: "#3D0099"
+		},
 		map: map,
 		title:"Hello World!"
+	});
+
+	var popup = new google.maps.InfoWindow({
+		content:'<div class="popup"><h2>City Name Here</h2>Testing...1...2...3...testing...1...2...3...</div>'
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		popup.open(map, marker);
 	});
 }
 
