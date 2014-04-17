dce5f4b160d4c9e4fd591c5f0b78021e0c627f02
Jesika Haria <jesikaharia@gmail.com>
2014-04-16 20:46:24 -0400

    Added Editor functionality TODO: Make it sliding div

--- wanderment/static/js/blur.js
+++ wanderment/static/js/blur.js
@@ -11,23 +11,45 @@ $(document).ready(function() {
 }).call(this);
 
 // Animate enter show post div 
-var showOrHide = true; 
+var showOrHide_Post = true; 
 $( "#show_sample_post" ).click(function() {
-  console.log("click");
-  if (showOrHide == true) {
-      showOrHide = false; 
+  console.log("click post");
+  if (showOrHide_Post == true) {
+      showOrHide_Post = false; 
       $( "#sample_post" ).animate({
         left: 0,
       }, 500, function() {
     });
   } else {
-    showOrHide = true; 
+    showOrHide_Post = true; 
     $( "#sample_post" ).animate({
         left: 2000,
       }, 500, function() {
     });
   }
 });
+
+// Animate enter show editor div
+// var showOrHide_Editor = true; 
+// $( "#show_editor" ).click(function() {
+
+//   console.log("click editor");
+//   if (showOrHide_Editor == true) {
+//       showOrHide_Editor = false; 
+//       $( "#editor" ).animate({
+//         left: 0,
+//       }, 500, function() {
+//     });
+//       console.log("animated in"); 
+//   } else {
+//     showOrHide_Editor = true; 
+//     $( "#editor" ).animate({
+//         left: 2000,
+//       }, 500, function() {
+//     });
+//     console.log("animated out"); 
+//   }
+// });
 
 }); 
 
--- wanderment/urls.py
+++ wanderment/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sample_post/', TemplateView.as_view(template_name="sample_post.html")),
+    url(r'^editor/', TemplateView.as_view(template_name="editor.html")),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
