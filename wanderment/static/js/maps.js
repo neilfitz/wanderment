4c0578a8455f82d24a3b6a1026cb7fff418cc4f0
Kevin Wu <sierangho@gmail.com>
2014-04-16 21:40:05 -0400

    Glowing for images
    Images glow on hover

--- wanderment/static/js/maps.js
+++ wanderment/static/js/maps.js
@@ -35,6 +35,7 @@ function initialize() {
 		imagePost.setAttribute('src', 'http://www.goldentriangle-tour-india.com/blog/wp-content/uploads/2013/11/Mumbai_city.jpg');
 		imagePost.setAttribute('height', '200');
 		imagePost.setAttribute('width', '300');
+		imagePost.setAttribute('class', 'glow')
 		imagePost.addEventListener('click', slide);
 
 		popupContent.appendChild(cityName);
