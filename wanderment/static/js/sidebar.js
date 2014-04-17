f5f598b2a0ce393e06e2222fbf69b19cf3eae7c8
Kevin Wu <sierangho@gmail.com>
2014-04-16 21:00:03 -0400

    Alignment on City data being passed in
    Also added function to generate inserted HTML (genTableInfo(dictionary) -
    create html from a dictionary)

--- wanderment/static/js/sidebar.js
+++ wanderment/static/js/sidebar.js
@@ -4,13 +4,23 @@ defaultInfo.info = "<p>search a city or navigate the map to start wandering<p>"
 	
 var sampleInfo = Object();
 sampleInfo.heading = "Mumbai";
-sampleInfo.info = "<p>Country: India</p><p>Population: 12,478,447</p><p>Elevation: 14m</p><p>Location Type:Big City</p><p>Language: Marathi, Hindi, English</p>"
+sampleDict = {"Country":"India","Population": "12,478,447", "Elevation": "14 m", "Type": "Big City", "Language": "Marathi, Hindi, English"};
+
 //data has a heading (string) and info section (html)
 function updateInfo(data){
     $("#sidebar-heading").text(data.heading);
     $("#sidebar-info").html(data.info);
-    $("#sidebar-info").addClass("data-list");
 }
+
+function genTableInfo(dict){
+	var html = '<table class="data-table">';
+	for (var key in dict){
+		html = html+'<tr><td>'+key+':</td><td>'+dict[key]+'</td></tr>';
+	}
+	html = html + '</table>';
+	return html;
+}
+sampleInfo.info = genTableInfo(sampleDict);
 
 $(document).ready(function(){
 	function searchMap(){
