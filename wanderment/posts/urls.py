3652e12bf6502c45d6f61593990749e6323ff238
Jesika Haria <jesikaharia@gmail.com>
2014-04-21 23:03:51 -0400

    /posts/1 works! Can go and view any post now

--- wanderment/posts/urls.py
+++ wanderment/posts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'), 
+    url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
 )
--- wanderment/posts/views.py
+++ wanderment/posts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from models import Post, City
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+  return HttpResponse("Hello, world. You're at the posts index.")
+
+def detail(request, post_id):
+  p = Post.objects.get(id=int(post_id))
+  template = loader.get_template('posts/detail.html')
+  context = RequestContext(request, {
+    'p' : p,
+    })
+  return HttpResponse(template.render(context))
