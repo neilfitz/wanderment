368821f3ab829446d216391ffc7df00fbee822a1
Jesika Haria <jesikaharia@gmail.com>
2014-04-25 13:48:40 -0400

    Started hooking up post creation, removed ugly post more posts thing

--- wanderment/posts/views.py
+++ wanderment/posts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.utils import timezone
+from django import forms
 
 from models import Post, City
+from forms import PostForm
 
 # From http://stackoverflow.com/questions/11811256/how-to-set-content-type-of-javascript-files-in-django 
 def java_script(request):
@@ -20,3 +23,19 @@ def detail(request, post_id):
     'p' : p,
     })
   return HttpResponse(template.render(context))
+
+def add_model(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+
+            # commit=False means the form doesn't save at this time.
+            # commit defaults to True which means it normally saves.
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('victory')
+    else:
+        form = PostForm()
+
+    return render(request, "create_post.html", {'form': form})
