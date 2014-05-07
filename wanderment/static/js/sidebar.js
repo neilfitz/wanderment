1b6b5389a5b5554e45eeb2c7d076c2548447b0a6
Anna <awaldo@mit.edu>
2014-05-07 15:45:30 -0400

    merge

--- wanderment/static/js/sidebar.js
+++ wanderment/static/js/sidebar.js
@@ -37,6 +37,12 @@ function resetInfo(){
     $("#sidebar-info").html(defaultInfo.info);
 }
 
+//var sideButtons = Object();
+//sideButtons.info = <button onclick="window.location.href='/editor'" class="emerald-flat-button" id="show_editor" style="margin-left: 45 px; margin-top: 50px; min-width:200px">Go To Editor</button> <button onclick="alert('No Saved Posts!')" class="emerald-flat-button" id="saved_posts" style="margin-left: 45 px; margin-top: 10px; min-width:200px">Saved Posts</button> <button onclick="slideNewPost()" class="emerald-flat-button" id="create_post" style="margin-left: 45 px; margin-top: 10px; min-width:200px">Create Post</button>;
+function afterLogin(){
+    $("#bottom-section").innerHTML(<button onclick="window.location.href='/editor'" class="emerald-flat-button" id="show_editor" style="margin-left: 45 px; margin-top: 50px; min-width:200px">Go To Editor</button> <button onclick="alert('No Saved Posts!')" class="emerald-flat-button" id="saved_posts" style="margin-left: 45 px; margin-top: 10px; min-width:200px">Saved Posts</button> <button onclick="slideNewPost()" class="emerald-flat-button" id="create_post" style="margin-left: 45 px; margin-top: 10px; min-width:200px">Create Post</button>);
+}
+
 // var locData = Object();
 // locData.mumbai = makeLatLon(18.975,72.825833);
 // locData.boston = makeLatLon(42.3133,-71.0571);
