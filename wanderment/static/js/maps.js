29ce24ba9e9f8ddd997beea6cf7ef3c20c5a90a5
Anna <awaldo@mit.edu>
2014-05-08 14:20:47 -0400

    please work

--- wanderment/static/js/maps.js
+++ wanderment/static/js/maps.js
@@ -143,7 +143,6 @@ function initialize() {
 			var imagePost = document.createElement('input');
 			imagePost.setAttribute('type', 'image');
 			imagePost.setAttribute('src', 'http://www.interasia.com.au/wp-content/uploads/2012/11/bangkok-temple-of-the-dawn.jpg');
-<<<<<<< HEAD
 			imagePost.setAttribute('height', '97');
 			imagePost.setAttribute('width', '140');
 			imagePost.setAttribute('class', 'glow2');
@@ -169,36 +168,7 @@ function initialize() {
 			var seeMore = document.createElement('p3');
 			seeMore.innerHTML = 'View More Posts';
 			seeMore.setAttribute('class', 'click-more');
-=======
-			imagePost.setAttribute('height', '50');
-			imagePost.setAttribute('width', '75');
-			imagePost.setAttribute('class', 'glow');
-			var imagePost2 = document.createElement('input');
-			imagePost2.setAttribute('type', 'image');
-			imagePost2.setAttribute('src', 'http://www.interasia.com.au/wp-content/uploads/2012/11/bangkok-temple-of-the-dawn.jpg');
-			imagePost2.setAttribute('height', '50');
-			imagePost2.setAttribute('width', '75');
-			imagePost2.setAttribute('class', 'glow');
-			var imagePost3 = document.createElement('input');
-			imagePost3.setAttribute('type', 'image');
-			imagePost3.setAttribute('src', 'http://www.interasia.com.au/wp-content/uploads/2012/11/bangkok-temple-of-the-dawn.jpg');
-			imagePost3.setAttribute('height', '50');
-			imagePost3.setAttribute('width', '75');
-			imagePost3.setAttribute('class', 'glow');
-			var imagePost4 = document.createElement('input');
-			imagePost4.setAttribute('type', 'image');
-			imagePost4.setAttribute('src', 'http://www.interasia.com.au/wp-content/uploads/2012/11/bangkok-temple-of-the-dawn.jpg');
-			imagePost4.setAttribute('height', '50');
-			imagePost4.setAttribute('width', '75');
-			imagePost4.setAttribute('class', 'glow');
-			
-			var seeMore = document.createElement('button');
-			seeMore.setAttribute('type', 'image5');
-			seeMore.setAttribute('src', '../imgs/view_more_posts.png');
-			seeMore.setAttribute('height', '380');
-			seeMore.setAttribute('width', '3000');
-			seeMore.setAttribute('class', 'glow5');
->>>>>>> b3370387b1361d6de90d90ff4fe549a83d4d308c
+
 
 			imagePost.addEventListener('click', function() {
 				alert('Nothing here yet! Take a look at Mumbai.')
