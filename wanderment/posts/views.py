ce673ba0c7a54f6fea00d91e149ed634b63f8eb6
Neil Fitzgerald <fitzgerald.neil@gmail.com>
2014-05-09 08:06:27 -0400

    Trying for redirect, v2.2

--- wanderment/posts/views.py
+++ wanderment/posts/views.py
@@ -39,7 +39,7 @@ def create_post(request):
 
             # Now call the index() view.
             # The user will be shown the homepage.
-            return HttpResponseRedirect('/posts/%i/' % my_file.pk)
+            return HttpResponseRedirect('/')
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
