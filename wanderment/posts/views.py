e1132cd698b27d98c39a01e828b129cfb0b2f0d2
Neil Fitzgerald <fitzgerald.neil@gmail.com>
2014-05-12 00:08:13 -0400

    Attempt 2 at redirecting to new post

--- wanderment/posts/views.py
+++ wanderment/posts/views.py
@@ -35,11 +35,11 @@ def create_post(request):
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
-            form.save(commit=True)
+            post = form.save(commit=True)
 
             # Now call the index() view.
             # The user will be shown the homepage.
-            return HttpResponseRedirect('/posts/' + form.cleaned_data['id'])
+            return HttpResponseRedirect('/posts/' + post.id)
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
